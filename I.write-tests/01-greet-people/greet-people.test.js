507ded84a0ea6d764cdc5a5e046d4bea35d396f2
folukeOpenuni <folukeagbede@googlemail.com>
2019-01-19 15:29:24 +0000

    test data to greet people

--- I.write-tests/01-greet-people/greet-people.test.js
+++ I.write-tests/01-greet-people/greet-people.test.js
@@ -1,7 +1,20 @@
-test('print list of names prefixed with Hello', function () {
-    // Arrange
+var greetPeople = require("./greet-people.js");
+test("print list of names prefixed with Hello", function() {
+  // Arrange
+  var names = ["Abdul", "Ahmad", "Paulina", "Ibraham", "Mariya"];
+  var unchanged = ["Abdul", "Ahmad", "Paulina", "Ibraham", "Mariya"];
+  var expected = [
+    "Hello Abdul",
+    "Hello Ahmad",
+    "Hello Paulina",
+    "Hello Ibraham",
+    "Hello Mariya"
+  ];
+  // var expected="Hello AbdulAhmadPaulinaIbrahamMariya"
 
-    // Act
-
-    // Assert
-})
+  // Act
+  var output = greetPeople(names);
+  // Assert
+  expect(output).toEqual(expected);
+  expect(names).toEqual(unchanged);
+});
