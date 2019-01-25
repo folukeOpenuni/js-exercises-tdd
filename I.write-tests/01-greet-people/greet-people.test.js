e437d5e4bfff72e6054d37a3d6f949370a05f16d
folukeOpenuni <folukeagbede@googlemail.com>
2019-01-25 17:32:12 +0000

    test data for greet people

--- I.write-tests/01-greet-people/greet-people.test.js
+++ I.write-tests/01-greet-people/greet-people.test.js
@@ -1,18 +1,11 @@
 var greetPeople = require("./greet-people.js");
 test("print list of names prefixed with Hello", function() {
-  // Arrange
-  var names = ["Foluke", "Ahmad", "Paulina", "Mary", "Mariya"];
-  // var unchanged = ["Foluke", "Ahmad", "Paulina", "Mary", "Mariya"];
-  var expected = [
-    "Hello Foluke",
-    "Hello Ahmad",
-    "Hello Paulina",
-    "Hello Mary",
-    "Hello Mariya"
-  ];
-  // Act
-  var output = greetPeople(names);
-  // Assert
-  expect(output).toEqual(expected);
-  //expect(names).toEqual(unchanged);
+  var arrOfNames = ["King", "Queen", "Jester"];
+  var unchanged = ["King", "Queen", "Jester"];
+  var expected = ["Hello King", "Hello Queen", "Hello Jester"];
+
+  //var output greetPeople(arrOfName);
+  var output = greetPeople(arrOfNames);
+  expect(output).toBe(expected);
+  expect(arrOfNames).toEqual(unchanged);
 });
--- I.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ I.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -1,10 +1,16 @@
-test('remove vowels from all words in array', function () {
-    // Arrange
+var removeVowels = require("./removeVowels-in-array.js");
 
-    // Act
+test("remove vowels from all words in array", function() {
+  // Arrange
+  var words = ["Irina", "Etza", "Daniel"];
+  var unchanged = ["Irina", "Etza", "Daniel"];
 
-    // Assert
-})
+  var expected = ["rn", "tz", "dnl"];
+  var output = removeVowels(words);
+
+  expect(words).toBe(unchanged);
+  expect(output).toBe(expected);
+});
 
 // example
 // input: ["Irina", "Etza", "Daniel"]
