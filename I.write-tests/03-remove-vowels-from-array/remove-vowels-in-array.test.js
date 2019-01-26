8c6524f6c94d2a90a3ac6fd27ae1fa824845c74a
folukeOpenuni <folukeagbede@googlemail.com>
2019-01-26 09:49:59 +0000

    test to remove vowel from any given words

--- I.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ I.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -1,15 +1,13 @@
-var removeVowels = require("./removeVowels-in-array.js");
-
+var removeVowels = require("./remove-vowels-in-array.js");
 test("remove vowels from all words in array", function() {
-  // Arrange
   var words = ["Irina", "Etza", "Daniel"];
   var unchanged = ["Irina", "Etza", "Daniel"];
 
   var expected = ["rn", "tz", "dnl"];
   var output = removeVowels(words);
 
-  expect(words).toBe(unchanged);
-  expect(output).toBe(expected);
+  expect(words).toEqual(unchanged);
+  expect(output).toEqual(expected);
 });
 
 // example
