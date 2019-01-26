895f3f07defff7d4592e4e1ffe2a9f54d692f9f6
folukeOpenuni <folukeagbede@googlemail.com>
2019-01-26 09:52:04 +0000

    test function to get second and third value

--- I.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ I.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -1,3 +1,15 @@
+var getSecondThird = require("./get-second-third.js");
+test("get second and third value", function() {
+  var input = [90, 5, 11, 8, 6];
+  var unchanged = [90, 5, 11, 8, 6];
+
+  var expected = [6, 8];
+  var output = getSecondThird(input);
+
+  expect(output).toEqual(expected);
+  expect(input).toEqual(unchanged);
+});
+
 // example
 // input = [90, 5, 11, 8, 6];
 // expected output = [6, 8];
