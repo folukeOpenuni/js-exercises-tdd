3258580d0c9d81f2a06442f0d07ecbb9b9706b58
folukeOpenuni <folukeagbede@googlemail.com>
2019-01-26 09:53:37 +0000

    test function to get even num

--- I.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ I.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,14 @@
+var getEven = require("./get-even-numbers.js");
+
+test("get even number", function() {
+  var num = [22, 13, 73, 82, 4];
+
+  var expected = [22, 82, 4];
+  var output = getEven(num);
+
+  expect(output).toEqual(expected);
+});
+
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
